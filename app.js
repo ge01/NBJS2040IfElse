a1dccda3c6f7a0cbc80cbf9501b7e3668c141e21
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-01 17:41:40 -0500

    if/else statement

---
+++ app.js
@@ -0,0 +1,11 @@
+/****************************************************
+*                 if/else Statement                 *
+****************************************************/
+var blue = 43;
+var red = 43;
+
+if (blue == red){
+  document.write("I love hotpockets");
+} else {
+  console.log("I love strawberries");
+}
